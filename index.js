5faae1000137471aea0462833dc238f029653087
Kerri Varner <kerrivarner@gmail.com>
2019-01-09 14:51:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ parameters and should return the string "Happy holidays!". */
   return "Happy holidays!";
 }
 happyHolidays();
-/* Write a function named happyHolidaysTo. This function should accept a parameter of the name of the person you want to wish happy holidays, and return the string `Happy holidays, ${name}! */
+/* Write a function named happyHolidaysTo. This function should accept a parameter
+of the name of the person you want to wish happy holidays, and return the string `Happy holidays, ${name}! */
 
 function happyHolidaysTo(name){
-
+  return `Happy holidays, ${name}!``
 }
 happyHolidaysTo("Kerri");
 
